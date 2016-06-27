6da566215ecef75e4128dfbfb5055174035dee83
Jon Church <me@jonchurch.com>
2016-06-27 19:54:16 -0400

    document ready!

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	response.sendFile('public/html/index.html', {root: __dirname})
+app.get('/', function(req, res) {
+    res.sendFile('public/html/index.html', { root: __dirname })
 });
 
-app.listen(port, function(req, res){
-	console.log('Now listening on port ... ' + port);
+app.listen(port, function(req, res) {
+    console.log('Now listening on port ... ' + port);
 });
