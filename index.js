9d6ab171945c88f6a015fe9c643a6c0ce0a84dd0
Jon Church <me@jonchurch.com>
2016-06-27 19:35:12 -0400

    server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+	response.sendFile('public/html/index.html', {root: __dirname})
+});
+
+app.listen(port, function(req, res){
+	console.log('Now listening on port ... ' + port);
+});
